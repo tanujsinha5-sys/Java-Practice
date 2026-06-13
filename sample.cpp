62158103a93b9a57500f9ba2458fd240d8a98d60
unknown <tanujsinha5@gmail.com>
2026-06-13 23:14:23 +0530

    adding code

---
+++ sample.cpp
@@ -0,0 +1,6 @@
+#include <iostream> // Include the input-output stream library
+int main() {
+   std::cout << "Hello, World!"; // Print the message to the console
+   return 0; // Return 0 to indicate successful execution
+}
+
